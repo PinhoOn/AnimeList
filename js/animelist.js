ea198d322b760cdcf1ac57b4274ab210d1942832
PinhoOn <pinhoonjob@gmail.com>
2021-03-29 15:36:28 -0300

    anime list fix

--- js/animelist.js
+++
@@ -1,4 +0,0 @@
-var animelist: ["https://m.media-amazon.com/images/M/MV5BMTY5ODk1NzUyMl5BMl5BanBnXkFtZTgwMjUyNzEyMTE@._V1_UX182_CR0,0,182,268_AL_.jpg" , "https://m.media-amazon.com/images/M/MV5BNzQyYzU3Y2MtOWY2Yy00ZGM2LTg3ZTUtMDJkZTJiMmEzMjYxXkEyXkFqcGdeQXVyMTI2NTY3NDg5._V1_UY268_CR16,0,182,268_AL_.jpg"]
-for (var i = 0; i < animelist.length; i++) {
-document.write("<img src=" + animelist[i] + ">")
-}
